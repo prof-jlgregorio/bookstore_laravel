f90bc21701fd6bc0b29fe95ad7ca5ad28f1e966f
Prof. J. L. Gregório <jlgregorio81@hotmail.com>
2025-04-25 20:23:30 -0300

    Basic of migrations OK - Create tables

---
+++ database/migrations/2025_04_25_224846_create_books_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50)->nullable(false);
+            $table->integer('year')->nullable(false);
+            $table->foreignId('author_id')->constrained();
+            $table->foreignId('gender_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
